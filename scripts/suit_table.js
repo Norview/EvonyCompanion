1d47d82ddace44eff6eb21b818fd4f9c5936f745
Norview <norview@evony803.info>
2024-12-18 23:31:18 -0800

    Buff aggregation in the comparison table.

--- scripts/suit_table.js
+++ scripts/suit_table.js
@@ -1,4 +1,4 @@
-// File: suite_table.js
+// File: suit_table.js
 //
 // A table for suit comparison
 
@@ -203,7 +203,9 @@ function SuitTable(/*function:*/restoreGenFunc) {
         const entry_class = ".ctentry";
         const _entry_index_class_prefix = "ctentry-";
         const entry_index_class_prefix = "." + _entry_index_class_prefix;
+        const c_buff_agg_row_id_prefix = "ct-buff-agg-";
         const c_buff_row_id_prefix = "ct-buff-";
+        const c_debuff_agg_row_id_prefix = "ct-debuff-agg-";
         const c_debuff_row_id_prefix = "ct-debuff-";
 
         const c_data_general_obj = "data_general";
@@ -215,6 +217,8 @@ function SuitTable(/*function:*/restoreGenFunc) {
         //   - 6 equipments
         //   - 12 buff values
         //   - 12 debuff values
+        //   - 3 aggregate buff values
+        //   - 3 aggregate debuff values
         function initialize(){
             // Reset to the default capacity
             var ctableCapSel = $("#comparison-capacity-select option");
@@ -224,7 +228,9 @@ function SuitTable(/*function:*/restoreGenFunc) {
         
             var equipments = [];
             var buffs = [];
+            var aggBuffs = [];
             var debuffs = [];
+            var aggDebuffs = [];
             var deleteRow = null;
             var restoreRow = null;
             var rows = $(c_comparison_table + " tr");
@@ -235,8 +241,12 @@ function SuitTable(/*function:*/restoreGenFunc) {
                     if (!!id){
                         if (id.startsWith("ct-equipment-")) {
                             equipments.push(child);
+                        } else if (id.startsWith(c_buff_agg_row_id_prefix)) {
+                            aggBuffs.push(child);
                         } else if (id.startsWith(c_buff_row_id_prefix)) {
                             buffs.push(child);
+                        } else if (id.startsWith(c_debuff_agg_row_id_prefix)) {
+                            aggDebuffs.push(child);
                         } else if (id.startsWith(c_debuff_row_id_prefix)) {
                             debuffs.push(child);
                         } else if (id === "ct-delete-row") {
@@ -248,6 +258,12 @@ function SuitTable(/*function:*/restoreGenFunc) {
                 });
             }
     
+            if (aggBuffs.length != 3) {
+                console.warn("Located " + aggBuffs.length + " aggregate buff rows in the comparison table instead 3.");
+            }
+            if (aggDebuffs.length != 3) {
+                console.warn("Located " + aggDebuffs.length + " aggregate debuff rows in the comparison table instead 3.");
+            }
             if (buffs.length != 12) {
                 console.warn("Located " + buffs.length + " buff rows in the comparison table instead 12.");
             }
@@ -255,7 +271,7 @@ function SuitTable(/*function:*/restoreGenFunc) {
                 console.warn("Located " + debuffs.length + " debuff rows in the comparison table instead 12.");
             }
             if (equipments.length != 6) {
-                console.warn("Located " + debuffs.length + " equipment rows in the comparison table instead 6.");
+                console.warn("Located " + equipments.length + " equipment rows in the comparison table instead 6.");
             }
             if (!deleteRow) {
                 console.warn("Couldn't locate the delete row in the comparison table.");
@@ -266,6 +282,8 @@ function SuitTable(/*function:*/restoreGenFunc) {
     
             return {
                 equipments : equipments,
+                aggBuffs: aggBuffs,
+                aggDebuffs: aggDebuffs,
                 buffs : buffs,
                 debuffs : debuffs,
                 deleteRow : deleteRow,
@@ -294,12 +312,49 @@ function SuitTable(/*function:*/restoreGenFunc) {
 
             return {
                 value : val,
-                type : idPart
+                type : idPart // EX: "groundAttack"
+            };
+        }
+        
+        // Return true if the given value is added into the aggregation; return false if the row is hidden.
+        function aggregateBuffs(value, agg, btype, aggBuffCtrls){
+            let btypelc = btype.toLowerCase();
+            let buffKinds = ["attack", "defense", "hp"];
+            for (let _buffKind of buffKinds) {
+                if (isNaN(agg[_buffKind])) {
+                    agg[_buffKind] = 0;
+                }
+                if (btypelc.indexOf(_buffKind) >= 0) {
+                    let trooptype = btypelc.substring(0, btypelc.length - _buffKind.length); // remove the suffix to get the troop type
+                    if (aggBuffCtrls[trooptype].data('excluded') !== true) {
+                        // If this troop type is not excluded, add it up
+                        agg[_buffKind] += value;
+                        return true;
+                    }
+                }
+            }
+            
+            // Not added into the aggregate
+            return false;
+        }
+        
+        function getAggBuff(aggBuffRow, agg, isBuff){
+            // ct-(de)buff-agg-attack-row
+            //                 |<-->|
+            //                buffkind ("attack" in this case)
+            let id = aggBuffRow.attr('id');
+            let prefix = isBuff ? c_buff_agg_row_id_prefix : c_debuff_agg_row_id_prefix;
+            let buffkind = id.substring(prefix.length, id.length - 4);
+            let rowName = "ct-" + (isBuff ? "buff-agg-" : "debuff-agg-") + buffkind + "-cell";
+            return {
+                value: agg[buffkind],
+                rowName : rowName
             };
         }
     
         // Populate the column for (de)buff properties
-        function addBuffs(buffRows, isBuff, buffs, index, refineBuffs){
+        function addBuffs(buffRows, aggBuffRows, aggBuffCtrls, isBuff, buffs, index, refineBuffs){
+            let agg = {};
             for (var buffRow of buffRows) {
                 var buffVal = getBuffInfoForRow(buffRow, buffs, isBuff);
                 var btype = buffVal.type;
@@ -316,9 +371,12 @@ function SuitTable(/*function:*/restoreGenFunc) {
                 }
                 var valStr = value + "%";
                 
+                var shown = aggregateBuffs(value, agg, btype, aggBuffCtrls);
+                
                 // Example:
-                // <td class="ctentry ctentry-0">15%</td>
-                var td = $("<td class=\"" + bgColor + "ctentry ctentry-" + index + "\">" + valStr + "</td>");
+                // <td class="ctentry ctentry-0 ct-buff-mountedAttack-cell row-vis-agg-factor">15%</td>
+                var rowName = "ct-" + (isBuff ? "buff-" : "debuff-") + btype + "-cell";
+                var td = $("<td class=\"" + bgColor + rowName + " row-vis-agg-factor ctentry ctentry-" + index + "\">" + valStr + "</td>");
 
                 var deco = getBuffDeco(function(){
                     return isGrey;
@@ -328,12 +386,27 @@ function SuitTable(/*function:*/restoreGenFunc) {
                 if (!!deco.isMax) {
                     td.css("fontWeight");
                 }
+                
+                // This row could be hidden now. Add the cell in hidden mode as well.
+                if (shown !== true) {
+                    td.hide();
+                }
 
                 buffRow.append(td);
             }
+            
+            let aggCellStylingClass = isBuff ? "buffaggcell" : "debuffaggcell";
+            for (var aggBuffRow of aggBuffRows) {
+                let aggBuff = getAggBuff(aggBuffRow, agg, isBuff);
+                let valStr = aggBuff.value + "%";
+                var td = $("<td class=\"" + aggCellStylingClass + " " + aggBuff.rowName + " ctentry ctentry-" + index + "\">" + valStr + "</td>");
+                td.css("fontWeight");
+                aggBuffRow.append(td);
+            }
         }
     
-        function updateBuffs(buffRows, columns, isBuff) {        
+        function updateBuffs(buffRows, aggBuffRows, aggBuffCtrls, columns, isBuff) {
+            let aggs = [];      
             var propName = isBuff ? "buffs" : "debuffs";
             // Update per row
             for (var buffRow of buffRows) {
@@ -354,6 +427,12 @@ function SuitTable(/*function:*/restoreGenFunc) {
                         value += (refineBuffs[btype] || 0);
                     }
                     
+                    let agg = aggs[_index];
+                    if (!agg) {
+                        aggs[_index] = agg = {};
+                    }
+                    aggregateBuffs(value, agg, btype, aggBuffCtrls);
+                    
                     td.text(value + "%");
                 
                     var isGrey = btype.startsWith("mounted") || btype.startsWith("siege");
@@ -367,18 +446,100 @@ function SuitTable(/*function:*/restoreGenFunc) {
                     }
                 });
             }
+
+            for (var aggBuffRow of aggBuffRows) {
+                var tds = aggBuffRow.find(entry_class);
+                tds.each(function(_index){
+                    var td = $(this);
+                    let aggBuff = getAggBuff(aggBuffRow, aggs[_index], isBuff);
+                    td.text(aggBuff.value + "%");
+                });
+            }
         }
-    
+        
+        function installAggButton(row, isBuff, ct) {
+            // Example: ct-debuff-groundAttack-row
+            const suffix = "Attack-row";
+            let id = row.attr('id');
+            if (id.endsWith(suffix)) {
+                let attackBuff = row;
+            
+                // Find the rows representing def and hp buff for the same troop category.
+                // ct-debuff-groundAttack-row (id) => ct-debuff-ground (stem)
+                let stem = id.substring(0, id.length - suffix.length);
+                let otherBuffs = attackBuff.siblings().filter(function() {
+                  return $(this).attr('id').startsWith(stem);
+                });
+                let allBuffs = attackBuff.add(otherBuffs);
+                            
+                let _ctrl = attackBuff.find(".row-vis-agg-ctrl");
+                _ctrl.on("click", function(){
+                    let _factors = allBuffs.find(".row-vis-agg-factor");
+                    let _img = _ctrl.children("img");
+                    let _src = _img.attr("src");
+
+                    let _excluded = !!_ctrl.data("excluded");
+                    if (_excluded) {
+                        // Reveal and enable agg
+                        _ctrl.removeData("excluded");
+                        _factors.show();
+                        _src = _src.replace("show.png", "hide.png");
+                    } else {
+                        // Hide and disable agg
+                        _ctrl.data("excluded", true);
+                        _factors.hide();
+                        _src = _src.replace("hide.png", "show.png");
+                    }
+            
+                    _img.attr("src", _src);
+                    
+                    ct.updateAll();
+                });
+                
+                // ct-debuff-ground (stem) => ground (troopType)
+                let lastDashIndex = stem.lastIndexOf('-');
+                let troopType = stem.substring(lastDashIndex +  1);
+            
+                return {
+                    ctrlItem : _ctrl,
+                    troopType : troopType // "ground"
+                };
+            }
+            
+            return null;
+        }
+        
         var obj = initialize();
     
         this._equipments = obj.equipments;
+        this._aggBuffs = obj.aggBuffs;
+        this._aggBuffCtrls = {};
+        this._aggDebuffs = obj.aggDebuffs;
+        this._aggDebuffCtrls = {};
+        // _aggBuffCtrls and _aggDebuffCtrls are dictionaries of trooptype~ctrlElem mapping.
         this._buffs = obj.buffs;
         this._debuffs = obj.debuffs;
         this._deleteRow = obj.deleteRow;
         this._restoreRow = obj.restoreRow;
         this._index = -1;
+        this._currentScenario = null;
+        
+        for (let _buff of this._buffs) {
+            let ctrlObj = installAggButton(_buff, true, this);
+            if (!!ctrlObj) {
+                this._aggBuffCtrls[ctrlObj.troopType] = ctrlObj.ctrlItem;
+            }
+        }
+        for (let _debuff of this._debuffs) {
+            let ctrlObj = installAggButton(_debuff, false, this);
+            if (!!ctrlObj) {
+                this._aggDebuffCtrls[ctrlObj.troopType] = ctrlObj.ctrlItem;
+            }
+        }
     
+        // Update the table with the given scenario. If scenario is not provided, reuse the current scenario.
         this.updateAll = function(scenario) {
+            this._currentScenario = scenario = (scenario || this._currentScenario);
             var delHeaders = this._deleteRow.find(entry_class);
         
             // Each contains the index of the column and the buffs objects
@@ -399,8 +560,8 @@ function SuitTable(/*function:*/restoreGenFunc) {
             });
         
             // Update
-            updateBuffs(this._buffs, columns, true);
-            updateBuffs(this._debuffs, columns, false);
+            updateBuffs(this._buffs, this._aggBuffs, this._aggBuffCtrls, columns, true);
+            updateBuffs(this._debuffs, this._aggDebuffs, this._aggDebuffCtrls, columns, false);
         };
     
         this.clearAll = function() {
@@ -490,10 +651,10 @@ function SuitTable(/*function:*/restoreGenFunc) {
             var refineBuffs = refiner.getBuffs(general);
             
             var buffs = general.getBuffs(scenario, c_starring_max).buffs;
-            addBuffs(this._buffs, true, buffs, index, refineBuffs);
+            addBuffs(this._buffs, this._aggBuffs, this._aggBuffCtrls, true, buffs, index, refineBuffs);
         
             var debuffs = general.getDebuffs(c_starring_max).buffs;
-            addBuffs(this._debuffs, false, debuffs, index, []);
+            addBuffs(this._debuffs, this._aggDebuffs, this._aggDebuffCtrls, false, debuffs, index, []);
         };
     }
 }
